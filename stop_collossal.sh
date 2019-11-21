c30e60a4361458accb58ecc64a0e8bc50e7b688b
Troy Fletcher <troyfletcher@protonmail.com>
2019-11-21 07:19:05 -0500

    added count up timing to stop collossal

--- stop_collossal.sh
+++ stop_collossal.sh
@@ -1,12 +1,19 @@
-while (true); 
-do
+date1=`date +%s`; 
+while true; do 
+clear
 figlet -m 2 -w 50 -f ~/Dropbox/colossal.flf ITS | lolcat
+echo -ne "$(date -d \@$((`date +%s` - $date1 + 18000)) +%H:%M:%S)      \r"; 
 sleep .3
+clear
 figlet -m 2 -w 50 -f ~/Dropbox/colossal.flf TIME | lolcat
+echo -ne "$(date -d \@$((`date +%s` - $date1 + 18000)) +%H:%M:%S)      \r"; 
 sleep .3
+clear
 figlet -m 2 -w 50 -f ~/Dropbox/colossal.flf TO | lolcat
+echo -ne "$(date -d \@$((`date +%s` - $date1 + 18000)) +%H:%M:%S)      \r"; 
 sleep .3
+clear
 figlet -m 2 -w 50 -f ~/Dropbox/colossal.flf STOP| lolcat
+echo -ne "$(date -d \@$((`date +%s` - $date1 + 18000)) +%H:%M:%S)      \r"; 
 sleep .3
 done
-
