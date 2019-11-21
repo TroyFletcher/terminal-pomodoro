7ab747f4728a1c776641cb40e0c42160b1cb546c
Troy Fletcher <troyfletcher@protonmail.com>
2019-11-21 07:09:44 -0500

    added count UP timers to the time to stop scripts so you can track how long youve ignored it

--- stop_tiny.sh
+++ stop_tiny.sh
@@ -1,7 +1,7 @@
 date1=`date +%s`; 
 while true; do 
     clear
-    tail -n 43 its_time_to_stop_ascii.txt |lolcat;
+    echo "stop"|figlet|lolcat;
     echo -ne "$(date -d \@$((`date +%s` - $date1 + 18000)) +%H:%M:%S)      \r"; 
     sleep 1;
 done;
