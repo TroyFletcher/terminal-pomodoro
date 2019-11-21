e14256e6927405695943c8443f7e8536a11f292c
Troy Fletcher <troyfletcher@protonmail.com>
2019-11-21 07:25:14 -0500

    why have an hour field when we're only doing minutes?

--- countdown.sh
+++ countdown.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 date1=$((`date +%s` + $1 * 60)); 
 while [ "$date1" -ge `date +%s` ]; do 
-  echo -ne "$(date -u --date @$(($date1 - `date +%s`)) +%H:%M:%S)\r";
+  echo -ne "$(date -u --date @$(($date1 - `date +%s`)) +%M:%S)\r";
   sleep 1
 done
