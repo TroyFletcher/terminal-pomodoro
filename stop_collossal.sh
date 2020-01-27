12a0d606925a1d405fad75602b2061b38fb1d479
Troy Fletcher <fletcher.troy@gmail.com>
2020-01-27 10:08:35 -0500

    fixed font reference

--- stop_collossal.sh
+++ stop_collossal.sh
@@ -1,19 +1,19 @@
 date1=`date +%s`; 
 while true; do 
 clear
-figlet -m 2 -w 50 -f ~/Dropbox/colossal.flf ITS | lolcat
+figlet -m 2 -w 50 -f colossal.flf ITS | lolcat
 echo -ne "$(date -d \@$((`date +%s` - $date1 + 18000)) +%H:%M:%S)      \r"; 
 sleep .3
 clear
-figlet -m 2 -w 50 -f ~/Dropbox/colossal.flf TIME | lolcat
+figlet -m 2 -w 50 -f colossal.flf TIME | lolcat
 echo -ne "$(date -d \@$((`date +%s` - $date1 + 18000)) +%H:%M:%S)      \r"; 
 sleep .3
 clear
-figlet -m 2 -w 50 -f ~/Dropbox/colossal.flf TO | lolcat
+figlet -m 2 -w 50 -f colossal.flf TO | lolcat
 echo -ne "$(date -d \@$((`date +%s` - $date1 + 18000)) +%H:%M:%S)      \r"; 
 sleep .3
 clear
-figlet -m 2 -w 50 -f ~/Dropbox/colossal.flf STOP| lolcat
+figlet -m 2 -w 50 -f colossal.flf STOP| lolcat
 echo -ne "$(date -d \@$((`date +%s` - $date1 + 18000)) +%H:%M:%S)      \r"; 
 sleep .3
 done
